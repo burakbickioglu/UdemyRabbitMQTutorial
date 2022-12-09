81415185fec21e68cebf16f6295bf7e1a400d02e
BurakBickioglu <burak.bickioglu@nextlua.com>
2022-12-09 14:16:09 +0300

    direct exchange

--- UdemyRabbitMQ.publisher/Program.cs
+++ UdemyRabbitMQ.publisher/Program.cs
@@ -14,15 +14,28 @@ using var connection = factory.CreateConnection();
 //kanalın oluşturulması
 var channel = connection.CreateModel();
 
-channel.ExchangeDeclare("logs-fanout", durable: true, type: ExchangeType.Fanout);
+channel.ExchangeDeclare("logs-direct", durable: true, type: ExchangeType.Direct);
 
-Enumerable.Range(1, 50).ToList().ForEach(x => {
-    string message = $"log {x}"; 
+Enum.GetNames(typeof(LogNames)).ToList().ForEach(x =>
+{
+    var routeKey = $"route-{x}";
+    var queueName = $"direct-queue-{x}";
+    channel.QueueDeclare(queueName, true, false, false);
+    channel.QueueBind(queueName, "logs-direct", routeKey,null);
+});
+
+Enumerable.Range(1, 50).ToList().ForEach(x =>
+{
+    LogNames log = (LogNames)new Random().Next(1,5);
+
+    string message = $"log-type: {log}";
     var messageBody = Encoding.UTF8.GetBytes(message);
 
+    var routeKey = $"route-{log}";
+
     //kuyruğa mesaj gönderme
-    channel.BasicPublish("logs-fanout", "", null, messageBody);
-    Console.WriteLine($"Mesaj gönderilmiştir -> {message}");
+    channel.BasicPublish("logs-direct", routeKey, null, messageBody);
+    Console.WriteLine($"Log gönderilmiştir -> {message}");
 
 });
 
@@ -31,5 +44,11 @@ Console.ReadLine();
 
 
 
-
+public enum LogNames
+{
+    Critical = 1,
+    Error = 2,
+    Warnings = 3,
+    Info = 4
+}
 
--- UdemyRabbitMQ.subcriber/Program.cs
+++ UdemyRabbitMQ.subcriber/Program.cs
@@ -12,29 +12,21 @@ using var connection = factory.CreateConnection();
 //kanalın oluşturulması
 var channel = connection.CreateModel();
 
-//random kuyruk adı alındı
-var randomQueueName = channel.QueueDeclare().QueueName;
-//durable : true ise fiziksel olarak sabit diskte kaydedilir, false ise ram de tutulur.
 
-// bir kuyruk exchange ye bind edildi, uygulama down olduğunda kuyruk ta down olacak
-channel.QueueBind(randomQueueName, "logs-fanout", "", null);
-
-// bir subcriber tek seferde kaç mesaj alacak bunun belirlenmesi
-//channel.ExchangeDeclare("logs-fanout", durable: true, type: ExchangeType.Fanout);
 channel.BasicQos(0, 1, false);
 
-//kuyruğun oluşturulması, eğer kuyruk zaten var ise sorun olmaz.
-//channel.QueueDeclare("hello-queue", true, false, false);
-
-
 var consumer = new EventingBasicConsumer(channel);
+var queueName = "direct-queue-Critical";
 
-channel.BasicConsume(randomQueueName, false, consumer);
+channel.BasicConsume(queueName, false, consumer);
 Console.WriteLine("Loglar dinleniyor.");
 consumer.Received += (sender, args) =>
 {
     var message = Encoding.UTF8.GetString(args.Body.ToArray());
     Console.WriteLine("Gelen mesaj : " + message);
+
+    //File.AppendAllText("log-critical.txt", message+ "\n");
+
     Thread.Sleep(1000);
     channel.BasicAck(args.DeliveryTag, false);
 };
