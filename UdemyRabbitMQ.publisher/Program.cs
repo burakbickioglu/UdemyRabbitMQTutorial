739de664dbbb9d5f0e81867eb06fc96fc5c68705
BurakBickioglu <burak.bickioglu@nextlua.com>
2022-12-09 11:10:58 +0300

    Add project files.

---
+++ UdemyRabbitMQ.publisher/Program.cs
@@ -0,0 +1,36 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+var factory = new ConnectionFactory();
+
+// canlı rabbitmq ya bağlanmak için
+//factory.Uri = new Uri("amqps://kozfvhtn:eRpeWdWSmW91rQ-DFvwVYnVpwsNfEF9j@woodpecker.rmq.cloudamqp.com/kozfvhtn");
+
+// dockerdaki rabbitmq ya bağlanmak için
+factory.HostName = "localhost";
+//bağlantının kurulması
+using var connection = factory.CreateConnection();
+
+//kanalın oluşturulması
+var channel = connection.CreateModel();
+
+//kuyruğun oluşturulması
+channel.QueueDeclare("hello-queue", true, false, false);
+
+Enumerable.Range(1, 50).ToList().ForEach(x => {
+    string message = $"Message {x}"; 
+    var messageBody = Encoding.UTF8.GetBytes(message);
+
+    //kuyruğa mesaj gönderme
+    channel.BasicPublish(string.Empty, "hello-queue", null, messageBody);
+    Console.WriteLine($"Mesaj gönderilmiştir -> {message}");
+
+});
+
+Console.ReadLine();
+
+
+
+
+
+
---
+++ UdemyRabbitMQ.subcriber/Program.cs
@@ -0,0 +1,37 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+var factory = new ConnectionFactory();
+//factory.Uri = new Uri("amqps://kozfvhtn:eRpeWdWSmW91rQ-DFvwVYnVpwsNfEF9j@woodpecker.rmq.cloudamqp.com/kozfvhtn");
+factory.HostName = "localhost";
+
+//bağlantının kurulması
+using var connection = factory.CreateConnection();
+
+//kanalın oluşturulması
+var channel = connection.CreateModel();
+
+// bir subcriber tek seferde kaç mesaj alacak bunun belirlenmesi
+channel.BasicQos(0, 1, false);
+
+//kuyruğun oluşturulması, eğer kuyruk zaten var ise sorun olmaz.
+channel.QueueDeclare("hello-queue", true, false, false);
+
+var consumer = new EventingBasicConsumer(channel);
+
+channel.BasicConsume("hello-queue", false, consumer);
+
+consumer.Received += (sender, args) =>
+{
+    var message = Encoding.UTF8.GetString(args.Body.ToArray());
+    Console.WriteLine("Gelen mesaj : " + message);
+    Thread.Sleep(1000);
+    channel.BasicAck(args.DeliveryTag, false);
+};
+
+Console.ReadLine();
+
+
+
+
